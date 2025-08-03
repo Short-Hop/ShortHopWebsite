5fa1b8b5169e3727cbf667b680905a3ad3e4a6ee
Luke Keller <lukeankeller@gmail.com>
2025-08-03 13:15:56 -0600

    add contact info

--- src/app/hero/hero.component.ts
+++ src/app/hero/hero.component.ts
@@ -2,13 +2,15 @@ import { Component } from '@angular/core';
 import {CommonModule} from "@angular/common";
 import {MatButtonModule} from "@angular/material/button";
 import {AlbumsComponent} from "../albums/albums.component";
+import {SocialsComponent} from "../socials/socials.component";
 
 @Component({
   selector: 'app-hero.component',
   imports: [
       CommonModule,
       MatButtonModule,
-      AlbumsComponent
+      AlbumsComponent,
+      SocialsComponent
   ],
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.scss'
